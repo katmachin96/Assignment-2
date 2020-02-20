3fbd4806565f13553d62625865e183815ce69358
katmachin96 <kathymachin96@hotmail.com>
2020-02-19 22:29:25 -0500

    javascript and styling chrono order responsive

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+//master set of tweets
 var tweetDatabase = []
+let tweetsIdSet = new Set()
+
 var pauseStatus = false
-var tweetArray = document.querySelector('.statuses')
+//var tweetArray = document.querySelector('.statuses')
 
-//stops tweet stream
-function stopSteam(){
+//used to filter tweets
+let searchInputBar = document.getElementById('searchBar')
+let searchButton = document.getElementById('searchButton')
+var searchString = ""
+var searchResult = []
 
-}
-console.log("SIZE OF INITIAL TWEET DATABASE" + tweetDatabase.length)
+//triggered when Search button is pressed
+searchButton.addEventListener('click', function(input){
+
+  searchString = searchInputBar.value
+
+  if(searchString == ""){
+    document.getElementById("response").innerHTML =
+    createContainer(tweetDatabase);
+  }else{
+    searchTweets(searchString.toLowerCase())
+  }
+})
 
 //gets and appends tweets to doc
 function getTweets(){
@@ -18,16 +34,24 @@ function getTweets(){
   .then(data => {
     let rawOutput = data.statuses
     console.log(rawOutput);
+    /*
+    **NOTE**
+    Since we are filtering duplicates tweets we don't have to worry about 'removing them'
+    ;however, to test that the filtering works just uncomment line 53, and comment outline 52
+    If all the tweets have the same id then they are "duplicates", so only one should show up
+    in the container.
+    */
     let refinedOutput = rawOutput.map(x => (
       {
         user: x.user.screen_name,
-        username: x.name,
+        username: x.user.name,
         profilePic: x.user.profile_image_url ,
-        time: timeAdjust(x.created_at),
+        time: formatDate(x.created_at),
+        // time: timeAdjust(x.created_at),
         text: x.text,
-        id: x.id}));
-
-    console.log("NEW TWEETS")
+        id: x.id
+        // id: 4
+      }));
 
 
   //adjusting time with moments.js
@@ -36,31 +60,25 @@ function getTweets(){
       // let newDate = moment(sortTime).format('LLL');
       return moment(time).format('LLL');
     }
-    // console.log(timeAdjust())
-
-  //chronological sorting attempt
-    function chronTweets() {
-      sortTime.sort(function(recent,oldest) {
-       recent = new Date(a.date).sortTime;
-       oldest = new Date(b.date).sortTime;
-       return recent>oldest ? 1 : -1;
-       console.log(chronTweets())
-      })
-}
     //push each element of new array into tweet database
     for (i = 0; i < refinedOutput.length ; i++){
-      // console.log("Current item")
+
+      // uncomment the line below if you wish to see the object before getting filtered
       // console.log(refinedOutput[i])
-      // console.log(refinedOutput[i]);
-      tweetDatabase.push(refinedOutput[i])
-      // console.log("CURRENT SIZE OF TWEET DATABASE:" + tweetDatabase.length)
+
+      //tweets with different ids get added - no duplicates
+      if(!(tweetsIdSet.has(refinedOutput[i].id))){
+        tweetsIdSet.add(refinedOutput[i].id)
+        tweetDatabase.push(refinedOutput[i])
+      }
     }
-    // document.getElementById("response").innerHTML =   JSON.stringify(tweetDatabase);
+    //sort tweets before displaying them
+    sortTweets(tweetDatabase)
+
+
     document.getElementById("response").innerHTML =
     createContainer(tweetDatabase);
   })
-
-
   .catch(err => {
     // error catching
     console.log(err)
@@ -84,21 +102,39 @@ document.getElementById("pause-bttn").addEventListener("click", function(){
   console.log(pauseStatus);
 })
 
-// No duplicate tweets
 
+//sorts tweets based on their date values, most recent to least recent
+function sortTweets(tweets){
+  tweets.sort((a,b) => b.time - a.time)
+}
 
+//gets a json time string and turns it into a date object
+function formatDate(time){
+  var date = new Date(time)
+  return date
+}
 
-//Search bar
-let searchInputBar = document.getElementById('searchBar')
-var searchString = ""
+//searches the tweets gathered so far based on a given substring
+function searchTweets(searchInput){
+  console.log("searching tweets with", searchInput);
+  //resets search results
+  searchResult = []
 
-searchInputBar.addEventListener('keyup', function(input){
-  var filter = searchInputBar.value
-  const term = input.target.value.toLowerCase()
+  //detects tweets with wanted string
+  for(i = 0; i < tweetDatabase.length; i++){
+    var tweetText = tweetDatabase[i].text
+    console.log(tweetText.toLowerCase().includes(searchInput), searchInput, tweetText);
+    if(tweetText.toLowerCase().includes(searchInput)){
+      searchResult.push(tweetDatabase[i])
+    }
+  }
 
-   console.log(input)
-})
+  //changes view to only filtered tweets
+  document.getElementById("response").innerHTML =
+    createContainer(searchResult);
 
+}
+//create the boundaries for multiple tweets to display
 function createContainer(tweetDatabase) {
   let contain = "<div class=\"maincontainer\">";
   for (let i = 0; i < tweetDatabase.length; i += 1) {
@@ -108,6 +144,20 @@ function createContainer(tweetDatabase) {
   contain += "</div>";
   return contain;
 }
+//makes date object into a presentable format (Mon, Day, Year, Time)
+function makeReadableDate(time){
+  var options = {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }
+  var timeString = time.toLocaleString('en-US', options);
+  return timeString
+}
+//defines the display parameters for an individual tweet
 function createTweet(tweet) {
   console.log(tweet);
 
@@ -116,15 +166,17 @@ function createTweet(tweet) {
       "<picture>" + "<source srcset\"tweet.profilePic\" type=\"image/svg+xml\">" +
       "<img class=\"tweetimg\" src=\"" +
        tweet.profilePic +
-      "\" alt=\"image not found\">"
+      "\" onerror= this.src=\"img/no_photo.jpg\" alt=\"image not found\">"
       + "</picture>" +"</div>" +
   "<div class=\"tweet-user\">" +
       tweet.user +"</div>" +
       "<div class=\"tweet-time\">" +
-      tweet.time +
+       makeReadableDate(tweet.time) +
       "</div>" +
       "<div class=\"tweet-text\">" +
       tweet.text +
       "</div>" +
   "</div>";
+
+
 }
